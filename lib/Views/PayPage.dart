db6ac0dad14973e8eaecc2696c9bcc8aee5b9c56
Abhishek Singh <anshuasr10@gmail.com>
2020-12-20 03:55:08 +0530

    Payment Mode added

---
+++ lib/Views/PayPage.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'PaymentService.dart';
+import 'package:progress_dialog/progress_dialog.dart';
+
+class PayPage extends StatefulWidget {
+  static String id = '/';
+  PayPage({Key key}) : super(key: key);
+  @override
+  _PayPageState createState() => _PayPageState();
+}
+
+class _PayPageState extends State<PayPage> {
+  onItemPress(BuildContext context, int index) async {
+    switch(index) {
+      default:
+        payViaNewCard(context);
+    }
+  }
+
+  payViaNewCard(BuildContext context) async {
+    ProgressDialog dialog = new ProgressDialog(context);
+    dialog.style(
+        message: 'Please wait...'
+    );
+    await dialog.show();
+    var response = await StripeService.payWithNewCard(
+        amount: '1000',
+        currency: 'INR'
+    );
+    await dialog.hide();
+    Scaffold.of(context).showSnackBar(
+        SnackBar(
+          content: Text(response.message),
+          duration: new Duration(milliseconds: response.success == true ? 1200 : 3000),
+        )
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    StripeService.init();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    ThemeData theme = Theme.of(context);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Payment Methods'),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: ListView.separated(
+            itemBuilder: (context, index) {
+              Icon icon;
+              Text text;
+
+              switch(index) {
+                default:
+                  icon = Icon(Icons.add_circle, color: theme.primaryColor);
+                  text = Text('Pay via new card');
+              }
+
+              return InkWell(
+                onTap: () {
+                  onItemPress(context, index);
+                },
+                child: ListTile(
+                  title: text,
+                  leading: icon,
+                ),
+              );
+            },
+            separatorBuilder: (context, index) => Divider(
+              color: theme.primaryColor,
+            ),
+            itemCount: 1
+        ),
+      ),
+    );
+  }
+}
