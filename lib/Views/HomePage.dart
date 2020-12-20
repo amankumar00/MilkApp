132f6d0b3e07f4f4cad93c49197074d455056f88
Abhishek Singh <anshuasr10@gmail.com>
2020-12-20 07:10:58 +0530

    build apk

--- lib/Views/HomePage.dart
+++ lib/Views/HomePage.dart
@@ -9,7 +9,7 @@ import 'dart:core';
 import 'package:firebase_auth/firebase_auth.dart';
 
 class HomePage extends StatefulWidget {
-  static String id = '/';
+  static String id = '/HomePage';
   @override
   _HomePageState createState() => _HomePageState();
 }
